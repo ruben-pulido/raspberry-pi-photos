ae2d57a32fa93a176bf35aa56b0647436c6a4e42
rubenpulido <ruben.pulido@liferay.com>
2018-05-20 03:53:34 +0200

    Read files backed up

--- display-images.sh
+++ display-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sleep 12
-sudo fbi --autozoom --blend 1000 --noverbose --vt 1 --timeout 10 /home/pi/Pictures/*.jp*g
+sudo fbi --autozoom --blend 1000 --noverbose --vt 1 --timeout 10 /home/pi/projects/photobak/photos_backup/googlephotos/darioberry1_at_gmail.com/*/*
